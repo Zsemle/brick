3a482aad2931fae5c1a7bd4cf8868edd6903e76b
Zsemle <bzsemle@gmail.com>
2020-10-23 22:53:43 +0200

    add mock data

---
+++ src/mocks/experiences.ts
@@ -0,0 +1,87 @@
+import { Experience } from '../types/brick-types'
+
+const experiences:Experience[] = [
+  {
+    name: 'Kitten Petting',
+    description: 'Pet a bunch of fluffy kittens while sipping coffee in Zwolle',
+    category: 'animals',
+    price: {
+      amount: 10,
+      currency: 'EUR'
+    }
+  },
+  {
+    name: 'Traffic Jam in Rotterdam',
+    description: 'Experience the daily struggle of suburban commuters! Car not included',
+    category: 'brain games',
+    price: {
+      amount: 42,
+      currency: 'EUR'
+    }
+  },
+  {
+    name: 'An afternoon as a Peasant',
+    description: 'Experience the everyday life of a 16th century Dutch farmer, living on the countryside of Zeeland. Activities include digging a hole in the mud, and peeling potatoes.',
+    category: 'sports',
+    price: {
+      amount: 64,
+      currency: 'EUR'
+    }
+  },
+  {
+    name: 'Cycling in Amersfoort',
+    description: 'Join the millions of native dutchies in their most popular activity!. Rental bike included - no helmet.',
+    category: 'sports',
+    price: {
+      amount: 30,
+      currency: 'EUR'
+    }
+  },
+  {
+    name: 'Language Coaching',
+    description: 'Spend an hour trying to pronounce the Dutch G without sounding like a Belgian, or worse.',
+    category: 'brain games',
+    price: {
+      amount: 12,
+      currency: 'EUR'
+    }
+  },
+  {
+    name: 'Go Shopping on a Horse',
+    description: 'Come to Den Bosch to experience the dream came true.',
+    category: 'animals',
+    price: {
+      amount: 246,
+      currency: 'EUR'
+    }
+  },
+  {
+    name: 'Watch the Brexit',
+    description: 'Take a seat on the beach at Zandvoort, to experience this historic moment. Chair included.',
+    category: 'brain games',
+    price: {
+      amount: 20,
+      currency: 'EUR'
+    }
+  },
+  {
+    name: 'Swimming in The Rijn at Utrecht',
+    description: 'Have you ever wanted to be seen as a real weird person? Sign up now for this activity!',
+    category: 'sports',
+    price: {
+      amount: 15,
+      currency: 'EUR'
+    }
+  },
+  {
+    name: 'Escape Groeningen',
+    description: 'Authentic 20th century post-apocalyptic style escape room, and a night full of activities!',
+    category: 'brain games',
+    price: {
+      amount: 45,
+      currency: 'EUR'
+    }
+  }
+]
+
+export default experiences
