f24d6365cb196f47abda0e045810683268dc1e0f
Zsemle <bzsemle@gmail.com>
2020-10-25 22:37:14 +0100

    make filtering work

--- src/Filter/Filter.test.tsx
+++ src/Filter/Filter.test.tsx
@@ -1,10 +1,17 @@
 import { shallow, ShallowWrapper } from 'enzyme'
 import React from 'react'
-import Filter from './Filter'
+import Filter, { FilterProps } from './Filter'
+import experiences from '../mocks/experiences'
 
 describe('the filter component', () => {
+  const props:FilterProps = {
+    experiences: experiences,
+    updateExperiences: jest.fn(),
+    filterCleared: jest.fn()
+  }
+  const wrapper:ShallowWrapper = shallow(<Filter{...props}/>)
+
   it('renders the component', () => {
-    const wrapper:ShallowWrapper = shallow(<Filter/>)
     expect(wrapper).toMatchSnapshot()
   })
 })
--- src/Filter/Filter.tsx
+++ src/Filter/Filter.tsx
@@ -1,7 +1,7 @@
 import React, { SyntheticEvent } from 'react'
 import { Experience, ExperienceCategory } from '../types/brick-types'
 
-interface FilterProps{
+export interface FilterProps{
   experiences: Experience[]
   updateExperiences: (experiences:Experience[]) => void
   filterCleared: () => void
@@ -9,7 +9,16 @@ interface FilterProps{
 
 interface FilterState{
   isOpen: boolean
+  applyTextFilter: boolean
+  applyPriceFilter: boolean
+  applyCategoryFilter: boolean
   filterText: string
+  filterCategories: ExperienceCategory[]
+  filterPriceMin: number
+  filterPriceMax: number
+  categoriesByText: Experience[]
+  categoriesByPrice: Experience[]
+  categoriesByCategory: Experience[]
 }
 
 const T:any = {
@@ -31,19 +40,112 @@ type Categories = {
 class Filter extends React.Component<FilterProps, FilterState> {
   constructor (props:any) {
     super(props)
-
+    const maxPrice = Math.max(...this.props.experiences.map(experience => experience.price.amount))
     this.state = {
       isOpen: false,
-      filterText: ''
+      filterText: '',
+      filterPriceMin: 0,
+      filterPriceMax: maxPrice,
+      filterCategories: [],
+      categoriesByText: [],
+      categoriesByPrice: [],
+      categoriesByCategory: [],
+      applyTextFilter: false,
+      applyPriceFilter: false,
+      applyCategoryFilter: false
     }
 
     this.onInputTextChange = this.onInputTextChange.bind(this)
+    this.onInputCheckboxChange = this.onInputCheckboxChange.bind(this)
+    this.onInputPriceMinChange = this.onInputPriceMinChange.bind(this)
+    this.onInputPriceMaxChange = this.onInputPriceMaxChange.bind(this)
+    this.compoundFilters = this.compoundFilters.bind(this)
   }
 
   private onInputTextChange (e:SyntheticEvent):void{
     e.persist()
-    const inputValue:string = (e.target as HTMLInputElement).value
-    this.setState({ filterText: inputValue }, this.filterByText)
+    this.setState(
+      {
+        filterText: (e.target as HTMLInputElement).value,
+        applyTextFilter: (e.target as HTMLInputElement).value.length > 0
+      },
+      this.filterByText
+    )
+  }
+
+  private onInputCheckboxChange (e:SyntheticEvent):void{
+    e.persist()
+    this.toggleFilterCategory(
+      (e.target as HTMLInputElement).value as ExperienceCategory
+    )
+  }
+
+  private onInputPriceMinChange (e:SyntheticEvent):void{
+    e.persist()
+    const newPriceLimit:number = (e.target as HTMLInputElement).value.length > 0 ? parseInt((e.target as HTMLInputElement).value) : 0
+    this.setState(
+      {
+        filterPriceMin: newPriceLimit,
+        applyPriceFilter: true
+      },
+      this.filterByPrice
+    )
+  }
+
+  private onInputPriceMaxChange (e:SyntheticEvent):void{
+    e.persist()
+    const newPriceLimit:number = (e.target as HTMLInputElement).value.length > 0 ? parseInt((e.target as HTMLInputElement).value) : 0
+    this.setState(
+      {
+        filterPriceMax: newPriceLimit,
+        applyPriceFilter: true
+      },
+      this.filterByPrice
+    )
+  }
+
+  private filterByPrice ():void{
+    const {
+      experiences
+    } = this.props
+    const {
+      filterPriceMin,
+      filterPriceMax
+    } = this.state
+    const filteredCategories:Experience[] = experiences.filter(
+      experience => experience.price.amount >= filterPriceMin && experience.price.amount <= filterPriceMax
+    )
+    this.setState({
+      categoriesByPrice: filteredCategories
+    }, this.compoundFilters)
+  }
+
+  private toggleFilterCategory (category:ExperienceCategory):void{
+    const { filterCategories } = this.state
+    let newFilterCategories:ExperienceCategory[] = [...filterCategories]
+    if (newFilterCategories.includes(category)) {
+      newFilterCategories = newFilterCategories.filter(item => item !== category)
+    } else {
+      newFilterCategories.push(category)
+    }
+    this.setState({
+      filterCategories: newFilterCategories,
+      applyCategoryFilter: newFilterCategories.length > 0
+    }, this.filterByCategories
+    )
+  }
+
+  private filterByCategories ():void {
+    const { filterCategories } = this.state
+    const {
+      experiences
+    } = this.props
+    const result = experiences.filter(
+      (experience) => filterCategories.includes(experience.category)
+    )
+    this.setState({
+      categoriesByCategory: result
+    }, this.compoundFilters)
   }
 
   private filterByText ():void {
@@ -52,8 +154,52 @@ class Filter extends React.Component<FilterProps, FilterState> {
     const result = experiences.filter(
       (experience) => experience.name.toLowerCase().includes(filterText.toLowerCase())
     )
+    this.setState({
+      categoriesByText: result
+    }, this.compoundFilters)
+  }
 
-    console.log(result)
+  private compoundFilters ():void {
+    const {
+      updateExperiences,
+      filterCleared
+    } = this.props
+    const {
+      categoriesByText,
+      categoriesByPrice,
+      categoriesByCategory,
+      applyTextFilter,
+      applyPriceFilter,
+      applyCategoryFilter
+    } = this.state
+    let result:Experience[] = []
+    if (applyTextFilter && !applyPriceFilter && !applyCategoryFilter) {
+      result = categoriesByText
+    }
+    if (!applyTextFilter && applyPriceFilter && !applyCategoryFilter) {
+      result = categoriesByPrice
+    }
+    if (!applyTextFilter && !applyPriceFilter && applyCategoryFilter) {
+      result = categoriesByCategory
+    }
+    if (applyTextFilter && applyPriceFilter && !applyCategoryFilter) {
+      result = categoriesByText.filter(experience => categoriesByPrice.includes(experience))
+    }
+    if (applyTextFilter && !applyPriceFilter && applyCategoryFilter) {
+      result = categoriesByText.filter(experience => categoriesByCategory.includes(experience))
+    }
+    if (!applyTextFilter && applyPriceFilter && applyCategoryFilter) {
+      result = categoriesByPrice.filter(experience => categoriesByCategory.includes(experience))
+    }
+    if (applyTextFilter && applyPriceFilter && applyCategoryFilter) {
+      const firstIntersection:Experience[] = categoriesByText.filter(experience => categoriesByPrice.includes(experience))
+      result = firstIntersection.filter(experience => categoriesByCategory.includes(experience))
+    }
+    if (!applyTextFilter && !applyPriceFilter && !applyCategoryFilter) {
+      filterCleared()
+      return
+    }
+    updateExperiences(result)
   }
 
   render () {
@@ -65,13 +211,15 @@ class Filter extends React.Component<FilterProps, FilterState> {
           <legend>{T.categoryFilterTitle}</legend>
           {Object.keys(categories).map((category, index) => (
             <label key={`${category}${index}`}>
-              <input type="checkbox"/>
+              <input type="checkbox" onChange={this.onInputCheckboxChange} value={category}/>
               <span>{category}</span>
             </label>
           )
           )
           }
         </fieldset>
+        <input min="0" type="number" value={this.state.filterPriceMin} onChange={this.onInputPriceMinChange}/>
+        <input min="0" type="number" value={this.state.filterPriceMax} onChange={this.onInputPriceMaxChange}/>
       </div>
     )
   }
